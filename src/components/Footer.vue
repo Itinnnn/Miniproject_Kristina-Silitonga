c09d2af2a9dcfe4c77f2c5a9727c286cecbec12a
itinnnn <kristinasilitonga825@gmail.com>
2022-05-17 22:49:46 +0700

    feat: fix footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,37 +1,20 @@
 <template>
-  <b-card-footer class="mt-5" footer-bg-variant="info">
-    <div class="container mb-5">
-      <div class="row" width="auto">
-        <div class="col md-5" align="left">
-          <h4>KS.Lab Scientific</h4>
-          <p>
-            Jl. Berdikari No 61A <br />
-            Pasar 1, Padang Bulan <Br /> Medan, Indonesia <br />
-          </p>
-        </div>
-        <div class="col md-5" align="right">
-          <p><strong>
-            CONTACTS </strong><br />
-            082168129023 <br />
-            kristinasilitonga825@gmail.com
-          </p>
-        </div>
-        <div class="col md-5" align="center">
-          <p><strong>
-            OUR PRODUCTS</strong> <Br /> Lab Furniture <br />
-            Lab Accessories <br />
-            Chemical Materials 
-          </p>
-        </div>
+  <footer class="mt-5 mb-5">
+      <hr />
+      <div class="container">
+          <div class="row">
+              <div class="col text-center">
+                  2022 Copyright KS.Lab Scientific
+              </div>
+          </div>
       </div>
-    </div>
-  </b-card-footer>
+  </footer>
 </template>
 
 <script>
 export default {
-  name: "FooterVue",
-};
+    name: 'FooterVue'
+}
 </script>
 
 <style>
